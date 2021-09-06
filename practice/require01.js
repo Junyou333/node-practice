0e9bc04d0569e056da6331d9a14947b9be4d87ca
Junyou333 <jyo336633@gmail.com>
2021-09-06 18:21:16 +0800

    node-practice

---
+++ practice/require01.js
@@ -0,0 +1,2 @@
+const f1 = require('./arrow-func');
+const f2 = require(__dirname)
---
+++ practice/writefile01.js
@@ -0,0 +1,16 @@
+const { error } = require('console');
+const fs =require('fs');
+
+const data={
+    name: 'David',
+    age:28
+};
+
+fs.writeFile('data.json',JSON.stringify(data,null,4),
+error=>{
+    if(error){
+        console.log('無法寫入',error);
+        process.exit;
+    }
+    console.log('寫入成功')
+});
