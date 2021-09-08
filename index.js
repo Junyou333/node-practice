35d0095d901df19a5113ce36f13a541dae3a888b
Junyou333 <jyo336633@gmail.com>
2021-09-08 14:39:17 +0800

    靜態內容:jquery, bootstrap

--- index.js
+++ index.js
@@ -2,12 +2,26 @@ require('dotenv').config(); //載入 .env的設定
 const express = require('express');
 const app = express();
 
+app.use('/', express.static('public'));
+app.use('/jquery', express.static('node_modules/jquery/dist'));
+app.use('/bootstrap', express.static('node_modules/bootstrap/dist'));
+
+
+// *** 路由定義開始
 app.get('/', function (req, res) {
     res.send('Hello World')
-})
+});
+// *** 路由定義結束
+
+//需放在所有路由之後
+app.use((req, res) => {
+    res.status(404).send(`<h1>找不到頁面</h1>`)
+});
+
 let port = process.env.PORT || 3000;
 
 app.listen(port, () => {
 
+    console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
     console.log(`啟動: ${port}`, Date());
 });
