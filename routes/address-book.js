6c25af36582c0983a0b4ca874cb7fc4d5b51cf6d
Junyou333 <jyo336633@gmail.com>
2021-09-16 16:08:51 +0800

    address-book 路由

---
+++ routes/address-book.js
@@ -0,0 +1,10 @@
+const { render } = require('ejs');
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+
+    res.render('address-book/main');
+});
+module.exports = router;
